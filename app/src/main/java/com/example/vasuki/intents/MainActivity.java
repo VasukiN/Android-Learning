3da7b79ac5381d010b15ea14fb95fc0f0e5ed3b4
VasukiN <vasuki.hebbar@gmail.com>
2018-01-07 13:32:22 +0530

    mini project
    small project creating activity,intents,simple list view.

---
+++ app/src/main/java/com/example/vasuki/intents/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.vasuki.intents;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button send;
+        send=(Button)findViewById(R.id.check);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("http://www.google.co.in"));
+                startActivity(i);
+            }
+        });
+
+    }
+}
